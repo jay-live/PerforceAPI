bdc2a7473b93267fab33a43d97d66a2d2247fb8d
Jay <80066463+jay-live@users.noreply.github.com>
2021-06-26 20:59:42 +0200

    PerforceAPI v0.1.1

--- Messages/Commands/Friend/Friend.java
+++ Messages/Commands/Friend/Friend.java
@@ -1,9 +1,13 @@
 package net.perforce.perforceapi.Messages.Commands.Friend;
 
+import net.perforce.perforceapi.Messages.Commands.Friend.Default.Default;
+import net.perforce.perforceapi.Messages.Commands.Friend.Errors.Errors;
+
 public class Friend {
 
 
-
+    public Default Default = new Default();
+    public Errors Errors = new Errors();
 
 
 }
